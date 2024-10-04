6b6de50150dfd84da1d5a9f28017337c61e11a70
unknown <egor-medvedev00@mail.ru>
2024-10-04 23:38:42 +0300

    create HW1 branch

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,12 +1,16 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.Month;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -15,9 +19,11 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         log.debug("redirect to users");
 
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("users.jsp");
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
+//      response.sendRedirect("users.jsp");
+
     }
 }
