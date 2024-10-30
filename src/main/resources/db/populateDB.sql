bdf0a2f05fa9e9c113df728c24819cbcbc384668
Egor Medvedev <egor-medvedev00@mail.ru>
2024-10-30 18:34:44 +0300

    add tests for MealService and JDBC work with DB

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,7 @@
 DELETE FROM user_role;
+DELETE FROM meals;
 DELETE FROM users;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +12,10 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO meals(dateTime, description, calories, user_id)
+VALUES ('2022-10-10 11:30:30', 'Breakfast', 200, 100000),
+       ('2022-10-10 14:30:00','Lunch', 500, 100000),
+       ('2022-10-10 19:30:00', 'Dinner', 1000, 100000);
