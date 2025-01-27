58a40583f9a273fd3fec963265dbb142cab5417f
Egor Medvedev <egor-medvedev00@mail.ru>
2025-01-27 23:51:17 +0300

    work Spring DATA JPA in process...

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -12,32 +12,42 @@ public class DataJpaMealRepository implements MealRepository {
 
     private final CrudMealRepository crudRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
+    private final CrudUserRepository crudUserRepository;
+
+    public DataJpaMealRepository(CrudMealRepository crudRepository, CrudUserRepository crudUserRepository) {
         this.crudRepository = crudRepository;
+        this.crudUserRepository = crudUserRepository;
     }
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        meal.setUser(crudUserRepository.getReferenceById(userId));
+        if (meal.isNew()) {
+            crudRepository.save(meal);
+        }
+
+        return get(meal.id(), userId) != null ? meal : null;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return get(id, userId) != null && crudRepository.delete(id, userId);
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Meal current = crudRepository.findById(id).orElse(null);
+
+        return current != null && current.getUser().getId() == userId ? current : null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return List.of();
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return List.of();
     }
 }
